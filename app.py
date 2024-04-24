5233006b416090bbd6e86916a52707e35bba57c6
ravindus0403 <147505042+ravindus0403@users.noreply.github.com>
2024-04-24 17:11:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ st.set_page_config(
 st.markdown(f'<style>{custom_css}</style>', unsafe_allow_html=True)
 
 # Logo image
-st.image("D:\Ravindu\Lecture Series\Projects\SPOTTER\Logo.png")
+st.image("Logo.png")
 
 
 st.title("SPOTTER")
